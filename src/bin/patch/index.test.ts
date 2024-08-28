54f6f49d56a6767fc0f73ad44656295d9e71c89d
Nikita Savchenko <zitros.lab@gmail.com>
2024-08-28 18:43:44 +0200

    More tests

--- src/bin/patch/index.test.ts
+++ src/bin/patch/index.test.ts
@@ -9,11 +9,13 @@ describe('dataunlocker patch', () => {
   const dir = resolve(process.cwd(), 'local');
   const file = resolve(dir, 'test.js');
   const backupFile = resolve('local/test.js.b80112c.backup');
+  const backupFile2 = resolve('local/test-manual.js.backup');
   const fileContent = 'console.log("test");';
 
   beforeEach(async () => {
     await rm(backupFile, { force: true });
     await rm(file, { force: true });
+    await rm(backupFile2, { force: true });
 
     await mkdir(dir, { recursive: true });
     await writeFile(file, fileContent);
@@ -53,5 +55,25 @@ describe('dataunlocker patch', () => {
       expect(e.exitCode).to.be.equal(1);
       expect(e.stderr).to.include('File not found');
     }
+  });
+
+  it("won't backup a file with --no-backup option", async () => {
+    await $({
+      stdio: ['inherit', 'inherit', 'inherit'],
+    })`npx -y dataunlocker patch local/test.js --no-backup`;
+
+    expect(await isFileExists(backupFile)).to.be.false;
+    expect(await readFile(file)).to.have.length.greaterThanOrEqual(3000);
+  });
+
+  it('backups a file to a selected location if given', async () => {
+    await $({
+      stdio: ['inherit', 'inherit', 'inherit'],
+    })`npx -y dataunlocker patch local/test.js --backup ${backupFile2}`;
+
+    expect(await isFileExists(backupFile2)).to.be.true;
+    expect(await isFileExists(backupFile)).to.be.false;
+    expect((await readFile(backupFile2)).toString()).to.be.equal(fileContent);
+    expect(await readFile(file)).to.have.length.greaterThanOrEqual(3000);
   });
 });
