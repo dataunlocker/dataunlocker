2e7abf8e08d14e10ef7d76df042c880bd5d347bc
Nikita Savchenko <zitros.lab@gmail.com>
2025-06-16 14:49:58 +0200

    Also pick DATAUNLOCKER_ID from package.json config

--- src/lib/utils/env.ts
+++ src/lib/utils/env.ts
@@ -1,2 +1,4 @@
 export const getEnv = (name: string): string | undefined =>
-  process.env[name] || process.env[`npm_config_${name.toLowerCase()}`];
+  process.env[name] ||
+  process.env[`npm_config_${name.toLowerCase()}`] ||
+  process.env[`npm_package_config_${name.toLowerCase()}`];
